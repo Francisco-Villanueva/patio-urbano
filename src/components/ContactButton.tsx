e385661db06c5637155bb2f531421eb681238f04
Francisco-Villanueva <franciscovillanuevaj99@gmail.com>
2024-04-15 15:47:06 -0300

    added phone number for whatsapp connection

--- src/components/ContactButton.tsx
+++ src/components/ContactButton.tsx
@@ -1,11 +1,17 @@
 import { WhatsAppIcon } from "../icons";
 
 export function ContactButton() {
+  const phoneNumber = "+59899584944";
+  const whatsappURL = `https://wa.me/${phoneNumber}`;
+
   return (
     <section className="fixed flex flex-col gap-4 bottom-0 right-0 m-6 ">
-      <button className=" bg-green text-white   h-14 w-14 rounded-full grid place-items-center ">
+      <a
+        className=" bg-green text-white   h-14 w-14 rounded-full grid place-items-center "
+        href={whatsappURL}
+      >
         <WhatsAppIcon />
-      </button>
+      </a>
     </section>
   );
 }
